c2108702da83e857142cf187e0c4574bbec6030c
Asra Siddiqui <asrasiddiqui127@gmail.com>
2019-10-09 22:27:49 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,10 +32,17 @@ return false;
 
 function drawGrid(){
   numCells = 20;
+  fillColor = 207;
+
   for(let i = 0; i <= width; i += width / numCells){
     for (let j = 0; j <= height; j += height / numCells) {
       rect(i, j, width / numCells, height / numCells);
-
+      if (fill === 207){
+        fill = 46;
+      } else {
+        fill = 207;
+      }
+      fill(fillColor);
     }
   }
 }
