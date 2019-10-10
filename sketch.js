0bbd0dfa418db0e3997ae10745c7dfea2ed71a61
Asra Siddiqui <asrasiddiqui127@gmail.com>
2019-10-09 22:00:14 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
+let noiseOffset = 0.0;
+let strokeWidth = 6;
+
 function setup() {
   createCanvas(800, 800);
   background(0);
-  strokeWeight(6);
+
 }
 
 function draw() {
+strokeWeight(strokeWidth);
+
+noiseOffset += 0.08;
+strokeWidth = noise(noiseOffset) * 100;
 
 if (mouseIsPressed){
   stroke(map(mouseX, 0, 600, 0, 255, true))
